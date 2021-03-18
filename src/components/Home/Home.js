bd0e69d1389b0ef18a9ae7a29470def5a71ab45e
Rahul Raina <rahulraina711@gmail.com>
2021-03-19 01:59:16 +0530

    added search functionality

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,18 +3,21 @@ import MovieCard from '../MovieCard/MovieCard';
 import axios from "axios";
 
 import './home.scss'
-import movieNames from '../MovieDetails/movielist';
 import ReactLoading from 'react-loading';
 import { Link } from 'react-router-dom';
 
 export default function Home (){
-
     const tmdbUrl = "https://api.themoviedb.org/3/movie/popular?api_key=da5b12deff4464ea34a1af0491d2207c";
+    const tmdbSearch = "https://api.themoviedb.org/3/search/movie?api_key=da5b12deff4464ea34a1af0491d2207c&query=";
+
+    const [apiUrl, setApiUrl] = useState(tmdbUrl);
+    
+    const [searchValue, setSearchValue] = useState("");
     const [movies, setMovies] = useState([]);
     const [loading, setLoading] = useState(true);
     const [topLoading, setTopLoading] = useState(false);
+    const [canLoad, setCanLoad] = useState(false);
     const [currentPage, setCurrentPage] = useState(1);
-    const [lucky, setLucky] = useState(false);
     
 
     useEffect(()=>{
@@ -23,8 +26,9 @@ export default function Home (){
 
     async function fetchmovies(){
         try{
-            const tmdbMovies = await axios.get(tmdbUrl)
+            const tmdbMovies = await axios.get(apiUrl);
             console.log(tmdbMovies.data);
+            tmdbMovies.data.total_pages > 1 ? setCanLoad(true) : setCanLoad(false);
             setMovies(tmdbMovies.data.results);
             setLoading(false);
             setTopLoading(false);
@@ -32,11 +36,35 @@ export default function Home (){
             console.log(err);
         }
     }
+
+    function onChangeHandler (e){
+        setSearchValue(e.target.value);
+        setApiUrl(tmdbSearch+e.target.value);
+        //console.log(e.target.value);
+    }
+
+    async function search(query){
+        setTopLoading(true);
+        
+        try{
+            const tmdbMovies = await axios.get(apiUrl);
+            console.log(tmdbMovies.data);
+            tmdbMovies.data.total_pages > 1 ? setCanLoad(true) : setCanLoad(false)
+            setMovies(tmdbMovies.data.results);
+            setLoading(false);
+            setTimeout(()=>{
+                setTopLoading(false);
+            },1000)
+
+        }catch(err) {
+            console.log(err);
+        }
+    }
     
-    async function loadMore(){
+    async function loadMore(url){
         setLoading(true);
         try{
-            const tmdbMovies = await axios.get(tmdbUrl+`&page=${currentPage+1}`)
+            const tmdbMovies = await axios.get(url+`&page=${currentPage+1}`)
             //console.log(tmdbUrl+`&page=${currentPage+1}`)
             //console.log(tmdbMovies.data);
             setCurrentPage(currentPage+1);
@@ -59,8 +87,8 @@ export default function Home (){
     return(
         <div className="data-field">
             <div className="top-btns">
-                <input placeholder="Search a movie here"/>
-                <button className="btn-def">Search</button>
+                <input placeholder="Search a movie here" value={searchValue} onChange={onChangeHandler}/>
+                <button className="btn-def" onClick={()=>{search(searchValue)}}>Search</button>
                 <Link to="/feelinglucky"><button className="btn-def">I m feeling lucky</button></Link>
             </div>
             
@@ -69,7 +97,7 @@ export default function Home (){
                 {renderMovies()}
             </div>
             {loading&&<ReactLoading type={"bars"} color={"grey"} />}
-            {!lucky&&<button className="btn-def" onClick={loadMore}>Load More</button>}
+            {canLoad&&<button className="btn-def" onClick={()=>{loadMore(apiUrl)}}>Load More</button>}
             <div className="footer-area">Made by Rahul Raina</div>
         </div>
     )
